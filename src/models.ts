400bd3646f6ccfb5a584f40c3b353169f408057c
Rene Klacan <rene@klacan.sk>
2023-05-24 12:53:30 +0200

    Working POC

--- src/models.ts
+++ src/models.ts
@@ -3,3 +3,5 @@ import type { InferModel } from "drizzle-orm";
 import type { mailingListUsers } from "../db/schema";
 
 export type MailingListUser = InferModel<typeof mailingListUsers>;
+export type NewMailingListUser = InferModel<typeof mailingListUsers, "insert">;
+
--- src/pages/api/mailing-list-users.ts
+++ src/pages/api/mailing-list-users.ts
@@ -1,10 +1,30 @@
+export const prerender = false;
 
 import type { APIRoute } from 'astro';
+import { drizzle } from "drizzle-orm/d1";
+import { getRuntime } from "@astrojs/cloudflare/runtime";
+
+import type { NewMailingListUser } from '../../models'
+import { mailingListUsers } from '../../../db/schema';
+
+export const client = (database: any) => drizzle(database);
 
 export const post: APIRoute = async ({ request }) => {
   if (request.headers.get("Content-Type") === "application/json") {
     const body = await request.json();
     const email = body.email;
+
+    const newUser: NewMailingListUser = {
+      email,
+      createdAt: new Date(),
+    };
+
+    const runtime = getRuntime(request) as any;
+    console.log(runtime.env.DB)
+    const db = drizzle(runtime.env.DB)
+
+    // await db.insert(mailingListUsers).values(newUser).run()
+    await db.insert(mailingListUsers).values(newUser).returning().get()
 
     return new Response(JSON.stringify({
       message: "Email: " + email
