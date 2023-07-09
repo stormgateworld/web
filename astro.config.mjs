46b691fcaed1063e0bbff489e7c89d3d62ad0857
Rene Klacan <rene@klacan.sk>
2023-07-09 22:08:26 +0200

    Storyblok POC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,29 @@
 import { defineConfig } from "astro/config"
 import cloudflare from "@astrojs/cloudflare"
 import tailwind from "@astrojs/tailwind"
+import { loadEnv } from "vite"
+import storyblok from "@storyblok/astro"
 
 import solidJs from "@astrojs/solid-js"
 
+const env = loadEnv("", process.cwd(), "STORYBLOK")
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()],
+  integrations: [
+    solidJs(),
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        "article-page": "components/storyblok/Article",
+        "article-overview-page": "components/storyblok/ArticleOverview",
+      },
+      apiOptions: {
+        region: "eu",
+      },
+    }),
+  ],
   output: "hybrid",
   adapter: cloudflare({
     mode: "directory",
